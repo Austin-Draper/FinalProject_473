a4cc39248e46368f48e27824dd20766dba2e6145
mhooper93 <32500890+mhooper93@users.noreply.github.com>
2018-05-13 11:28:21 -0700

    merging my changes with master

--- chat-app/app/routes/login.js
+++ chat-app/app/routes/login.js
@@ -5,16 +5,14 @@ import { inject } from '@ember/service';
 export default Route.extend({
   session: inject(),
   beforeModel: function() {
+      if (this.get('session.isAuthenticated')) {
+        this.transitionTo('index');
+      }
     return this.get('session').fetch().catch(function() {});
   },
 
   actions: {
     logIn() {
-      //logout if currently logging in
-      if (this.get('session.isAuthenticated')) {
-        this.get('session').close();
-      }
-      //login
       this.get('session').open('firebase', {
         provider: 'password',
         email: this.controller.get('userEmail'),
@@ -25,12 +23,36 @@ export default Route.extend({
           this.transitionTo('index');
         }.bind(this));
 
+
       //alert(`Saving of the following email address and Message is in progress: ${this.get('emailAddress')} - ${this.get('message')}`);
       //this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}: ${this.get('message')}`);
       //this.set('message', '');
     },
+
     logOut() {
       this.get('session').close();
     }
   }
 });
+
+// export default Route.extend({
+//   actions: {
+//     logIn() {
+//       var controller = this.get('controller');
+//       var email = controller.get('userEmail');
+//       var password = controller.get('userPassword');
+//
+//       this.get('session').open('firebase', {
+//         provider: 'password',
+//         email: email,
+//         password: password
+//       }).then(function() {
+//         this.transitionTo('index');
+//       }.bind(this));
+//     }
+//   },
+//
+//   logOut() {
+//     this.get('session').close();
+//   }
+// });
